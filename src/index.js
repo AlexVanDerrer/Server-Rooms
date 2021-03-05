9481882e31f4fa509bbb4d5b991731f0bf4d202e
AlexVanDerrer <borisow.a.s@yandex.ru>
2021-03-05 13:32:32 +0300

    Рефакторинг

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const redis = require('socket.io-redis');
+// const redis = require('socket.io-redis');
 const users = require('./users')();
 const cryptoJS = require('crypto-js');
 
-io.adapter(redis({ host: 'localhost', port: 6379 }));
+// io.adapter(redis({ host: 'localhost', port: 6379 }));
 
-const key = cryptoJS.MD5('rooms-security').toString();
+// const key = cryptoJS.MD5('rooms-security').toString();
 function createObjForMsg(name, text, id, type, avatar) {
     return {
         name, 
-        text: cryptoJS.AES.encrypt(text, key).toString(), 
+        // text: cryptoJS.AES.encrypt(text, key).toString(), 
+        text,
         id, 
         type, 
         avatar
     }
 };
-const adminNickname = cryptoJS.AES.encrypt('AdminBot', key).toString();
+const adminNickname = 'AdminBot'; //cryptoJS.AES.encrypt('AdminBot', key).toString();
 const adminAvatar = 'https://purepng.com/public/uploads/medium/purepng.com-anonymous-maskanonymous-maskfawkesgunpowder-plored-cheeksguy-fawkes-mask-1421526668310teu48.png'
 
 
@@ -31,6 +32,8 @@ io.on('connection', socket => {
             return cb('fail data')
         }
         const countUserInRoomNow = users.getByRoom(dataUserFront.room).length;
+        const KEY = cryptoJS.MD5(dataUserFront.room).toString();
+
 
         socket.join(dataUserFront.room);
         users.remove(socket.id)
@@ -56,7 +59,12 @@ io.on('connection', socket => {
         
         socket.broadcast
             .to(dataUserFront.room)
-            .emit('newMessage', createObjForMsg(adminNickname, `${dataUserFront.name} in room `, socket.id, 'received', adminAvatar))
+            .emit('newMessage', createObjForMsg(
+                adminNickname, 
+                `${dataUserFront.name} in room `, 
+                socket.id, 
+                'received', 
+                adminAvatar))
     })
     /**
      * Обработчик сообщений пользователей
@@ -66,9 +74,9 @@ io.on('connection', socket => {
         if (!data.text){
             return cb('empty text');
         }
-        console.log(data);
+        // console.log(data);
         const message = {
-            name: cryptoJS.AES.encrypt(user.name, key).toString(),
+            name: cryptoJS.AES.encrypt(user.name, user.room).toString(),
             text: data.text,
             id: data.id,
             type: 'received',
@@ -76,7 +84,6 @@ io.on('connection', socket => {
         }
         if (user) {
             io.to(user.room).emit('newMessage', message);
-            console.log(message);
         }
         cb()
         
